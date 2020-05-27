7e5f3fd2fc85a6519e8cc933abee66d3fafe7336
artywhite <jeronimo2white@gmail.com>
2020-05-26 23:54:29 -0700

    a little logging

--- launcher.py
+++ launcher.py
@@ -4,7 +4,8 @@ from sbs_course import StepByStepCourse
 all_bots = db_module.get_bots_ids()
 
 started_bots = []
-for each in all_bots:
-    new_bot = StepByStepCourse(each)
+for bot_id in all_bots:
+    new_bot = StepByStepCourse(bot_id)
     new_bot.start_bot()
+    print(f"Bot '{bot_id}' started")
     started_bots.append(new_bot)
